92f1f03a1370644093b0acab641a49bb1e2905a4
asas <d>
2012-09-10 20:56:46 -0500

    changed version number and the url to point to github account

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='HelloShopply',
-    version='0.0.1',
+    version='0.0.2',
     author='Shopply',
     author_email='contactus@shopply.com',
     packages=['helloshopply.code', 'helloshopply.test'],
     scripts=[],
-    url='http://pypi.python.org/pypi/MyFirstPackageCreation1/',
+    url='https://github.com/anandrjoshi/helloshopply/',
     license='LICENSE.txt',
-    description='My First Package Creation',
+    description='Hello Shopply',
     long_description=open('README.txt').read(),
     install_requires=[
         "pyes >= 0.19.1",
